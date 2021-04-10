17c0e19706cfc55202870640a35733f3b15cdffd
naisthorpe <aisthorpe@gmail.com>
2021-04-10 15:53:17 -0400

    Pseudocode the notes post method

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post("/api/notes", (req, res) => {
 });
 
 // route for wildcard root
-app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
 
 // Start server to begin listening
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
